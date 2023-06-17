49d9cddf8659724522b999f5e9a58456a8316dc8
Edel <edelmedina322@gmail.com>
2023-06-17 19:40:33 -0300

    Add default main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Nav from "./Components/Nav";
 import CheatsheetOverview from "./Components/CheatsheetOverview";
+import DefaultMain from "./Components/DefaultMain";
 
 function App() {
+  const user = false;
   return (
     <div className="App">
       <Nav />
-      <CheatsheetOverview />
+      <main>{user ? <CheatsheetOverview /> : <DefaultMain />}</main>
     </div>
   );
 }
---
+++ src/Components/DefaultMain.js
@@ -0,0 +1,17 @@
+const DefaultMain = () => {
+  return (
+    <div className="default-main wrapper">
+      <h1>
+        All the notes you need, at your fingertips,
+        <br />
+        <span className="accent-text">Easier</span> than ever
+      </h1>
+      <p>
+        Access all your essential notes effortlessly and stay ahead of the game.
+      </p>
+      <p>Simplify your learning journey like never before.</p>
+    </div>
+  );
+};
+
+export default DefaultMain;
